f9f6c3e6b88fa27e764add76c93d7ce6aa668efb
Mat Jones <mjones44@ycp.edu>
2016-09-19 17:50:31 -0400

    Started working on ROS Package Management utility software in Java

---
+++ tools/ROS-package-utility/src/edu/ycp/ros/packageUtility/ErrorStrings.java
@@ -0,0 +1,6 @@
+package edu.ycp.ros.packageUtility;
+
+public class ErrorStrings {
+	public static String ROS_INSTALL_NOT_FOUND = "Failed to find ROS install in default directory. Prompting user to locate ROS install directory.";
+	public static String OPERATION_CANCELLED = "Operation cancelled by user.";
+}
