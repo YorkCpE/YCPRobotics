b6f57ed30c66d95fba4d95f10d5ab33ba4f11d57
Jason Forsyth <jbforsyth@ycp.edu>
2016-10-02 19:04:39 -0400

    Initial commit of workspace and organization
    -Initial commit of workspace from Cara
    -Creation of packages directory to store relevant ROS packages
    -Removed .git folder and .gitignore from Turtlebot packages

---
+++ workspace/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/carasperbeck/ros_ws/src
+# Build directory: /home/carasperbeck/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(robot_navigation/aau_multi_robot-master/adhoc_communication)
+SUBDIRS(robot_navigation/aau_multi_robot-master/map_merger)
+SUBDIRS(agitr/double_sim)
+SUBDIRS(agitr/fast_yellow)
+SUBDIRS(agitr/FirstLaunchFile)
+SUBDIRS(agitr/hello)
+SUBDIRS(agitr/publevel)
+SUBDIRS(agitr/subpose)
+SUBDIRS(robot_navigation/aau_multi_robot-master/explorer)
---
+++ workspace/build/src/learning_joy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/carasperbeck/ros_ws/src/src/learning_joy
+# Build directory: /home/carasperbeck/ros_ws/build/src/learning_joy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
