643cb01418d40bc1cb424f82cc4c56c302b9f7dc
Mat Jones <mjones44@ycp.edu>
2016-11-30 13:19:36 -0500

    fixed bug in script

--- tools/OpenCV-HSV-Color-Tool/HSVColorTool.py
+++ tools/OpenCV-HSV-Color-Tool/HSVColorTool.py
@@ -66,12 +66,9 @@ if __name__ == "__main__":
 
             # find contours in the masked image and keep the largest one
             cnts = None
-            if cv2.__version__ == "3.0.0":
-                (cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            elif cv2.__version__ == "3.1.0":
+            (cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if cv2.__version__ == "3.1.0":
                 (_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            else:
-                raise RuntimeError("OpenCV version 3.0.0 or 3.1.0 required! Installed version: " + cv2.__version__)
             if cnts:
                 c = max(cnts, key=cv2.contourArea)
 
